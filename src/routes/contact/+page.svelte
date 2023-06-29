50c4843a4a7b07dd14b20c3a583c4d74be5a4b5c
Michael McDonald <mikemcdonald83@gmail.com>
2023-06-29 07:01:43 -0500

    forms: added school key, fixed req

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -30,7 +30,7 @@
 	</AutoGrid>
 	<Title title="Location" />
 	<div class="map-container">
-		<!-- <iframe
+		<iframe
 			title="Embedded Google map for the church and school."
 			src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13766.1874799546!2d-87.0455735!3d30.3922225!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8890db5cd86af007%3A0x81daadf374b2d0a1!2sGood%20Shepherd%20Lutheran%20Church%20and%20School!5e0!3m2!1sen!2sus!4v1687336466237!5m2!1sen!2sus"
 			width="600"
@@ -38,18 +38,18 @@
 			style="border:0;"
 			loading="lazy"
 			referrerpolicy="no-referrer-when-downgrade"
-		/> -->
+		/>
 	</div>
 </MainBlock>
 
 <MainBlock maxWidth="90ex">
 	<Title title="Email Us" />
-	<Form formId="contact-page-email">
-		<UserName />
+	<Form formId="contact-page-email" reqExplain="">
+		<UserName required={false} />
 		<UserEmail />
 		<Select name="recipient" label="To">
-			<option value="church">church</option>
-			<option value="school">school</option>
+			<option value="church">Church</option>
+			<option value="school">School</option>
 		</Select>
 		<Subject />
 		<TextArea name="body" />
